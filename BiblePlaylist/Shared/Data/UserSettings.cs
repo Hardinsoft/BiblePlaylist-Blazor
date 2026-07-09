f6ef053e652c13439265769071a566dbb7db933b
Greg Hardin <greg.hardin@live.com>
2026-07-08 18:23:54 -0700

    Implement TTS for playlist description and BookChapter/Segment VoiceText before audio playback. Added speakSequenceAsync and speakTextAsync in JS. Integrated into PlayCurrentSegment with EnableTts setting.

--- BiblePlaylist/Shared/Data/UserSettings.cs
+++ BiblePlaylist/Shared/Data/UserSettings.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,5 +10,6 @@ namespace BiblePlaylist.Shared.Data
     {
         public bool Autoplay { get; set; }
         public bool Repeat { get; set; }
+        public bool EnableTts { get; set; } = true;
     }
 }
